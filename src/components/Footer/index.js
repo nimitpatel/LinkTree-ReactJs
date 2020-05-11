18ada1f692e89844bf9845df5070d99534d8de6e
nimit patel <nimit2997@gmail.com>
2020-05-11 22:44:15 +0530

    Footer Fix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ const index = () => {
     return (
         <>
             <footer>
-                <p>Developed with <span className="iconify coffee" data-inline="false" data-icon="cib:buy-me-a-coffee"></span> by <span><a>@nimit.codes</a></span></p>
+                <p>Developed with <span className="iconify coffee" data-inline="false" data-icon="cib:buy-me-a-coffee"></span> by <span><a href="https://instagram.com/nimit.codes">@nimit.codes</a></span></p>
             </footer>
         </>
     )
